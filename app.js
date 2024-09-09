b43cfd74c26758e534727a735897a84d7b161083
NICOLASRENGIFO24 <nicolaseggo@gmail.com>
2024-09-09 07:56:36 -0500

    primer clase javascrypt

---
+++ app.js
@@ -0,0 +1,21 @@
+ console.log("Hola Mundo");
+
+ //variables
+ let numero1= 45;
+ let numero2= 39;
+ const pi= 3.141516;
+ let estado= true;
+ 
+
+let suma;
+
+suma=numero1+numero2;
+
+console.log(suma);
+
+// Concatenar valores
+
+console.log("el primer valor es" + numero1);
+console.log("el segundo valor es" + numero2);
+console.log("la suma de : " + numero1 + " y " + numero2 + " es " + suma); 
+console.log(`la suma de ${numero1} y ${numero2} es ${suma}`)
